2ae614c375fd8a099190775f15f42c54adaf86fd
Kohei Yoshino <kohei.yoshino@gmail.com>
2023-11-16 23:20:07 -0500

    Add label option to grid/table row group

--- src/lib/components/grid/grid-body.svelte
+++ src/lib/components/grid/grid-body.svelte
@@ -5,25 +5,49 @@
   @see https://w3c.github.io/aria/#rowgroup
 -->
 <script>
+  import { getRandomId } from '$lib/services/util';
+
   /**
    * The `class` attribute on the wrapper element.
    * @type {string}
    */
   let className = '';
   export { className as class };
+
+  /**
+   * Display label for the row group.
+   * @type {string}
+   */
+  export let label = '';
+
+  const id = getRandomId('tbody');
 </script>
 
 <div
   role="rowgroup"
-  class="sui grid-body {className}"
+  class="sui grid-body rowgroup {className}"
+  aria-labelledby={label ? `${id}-label` : undefined}
   aria-roledescription="grid body"
   {...$$restProps}
 >
+  {#if label}
+    <tr>
+      <th id="{id}-label" colspan="9999" scope="rowgroup">{label}</th>
+    </tr>
+  {/if}
   <slot />
 </div>
 
 <style lang="scss">
   .grid-body {
     display: table-row-group;
+  }
+
+  th {
+    padding: 8px;
+    color: var(--sui-secondary-foreground-color);
+    background-color: var(--sui-secondary-background-color);
+    font-size: var(--sui-font-size-default);
+    text-align: left;
   }
 </style>
