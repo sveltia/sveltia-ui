2600678d498e68ac71f4e641b80aa6d9f89d248b
Kohei Yoshino <kohei.yoshino@gmail.com>
2025-11-29 23:03:03 -0500

    Prevent elements from being pushed up by virtual keyboard on mobile

--- src/lib/components/util/app-shell.svelte
+++ src/lib/components/util/app-shell.svelte
@@ -70,7 +70,15 @@
   @see https://stackoverflow.com/q/2989263
 -->
 <svelte:head>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <meta
+    name="viewport"
+    content={[
+      'width=device-width',
+      'initial-scale=1',
+      'maximum-scale=1',
+      'interactive-widget=resizes-content',
+    ].join(', ')}
+  />
   <meta name="google" content="notranslate" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
