8f57928a04ff26dc18288b7ee188b225dd6afb5a
Kohei Yoshino <kohei.yoshino@gmail.com>
2026-07-11 23:03:51 -0400

    Move SvelteKit config into vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
 /// <reference types="vitest/config" />
 
 import yaml from '@rollup/plugin-yaml';
+import adapter from '@sveltejs/adapter-auto';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltePreprocess } from 'svelte-preprocess';
 import { defineConfig } from 'vite';
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [sveltekit(), yaml()],
+  plugins: [
+    sveltekit({
+      preprocess: sveltePreprocess(),
+      adapter: adapter(),
+      compilerOptions: {
+        runes: true,
+      },
+    }),
+    yaml(),
+  ],
   test: {
     environment: 'happy-dom',
     include: ['src/lib/{components,services}/**/*.test.js'],
