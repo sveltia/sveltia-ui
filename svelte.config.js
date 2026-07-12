cea5e44a05280d12459a6fe426e16d216f329c6a
Kohei Yoshino <kohei.yoshino@gmail.com>
2026-07-11 23:09:02 -0400

    Revert "Move SvelteKit config into vite.config.js"
    This reverts commit 8f57928a04ff26dc18288b7ee188b225dd6afb5a.

---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from '@sveltejs/adapter-auto';
+import { sveltePreprocess } from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: sveltePreprocess(),
+  kit: {
+    adapter: adapter(),
+  },
+  compilerOptions: {
+    runes: true,
+  },
+};
+
+export default config;
