84dc29459a7821e6a5675e2016083342a659b22b
Kohei Yoshino <kohei.yoshino@gmail.com>
2026-07-12 12:09:42 -0400

    Rename i18n resources export to strings

--- src/lib/services/i18n.js
+++ src/lib/services/i18n.js
@@ -1,21 +1,21 @@
 import { addMessages, init } from '@sveltia/i18n';
 
-const rawResources = import.meta.glob('../locales/*.yaml', { eager: true, import: 'default' });
+const resources = import.meta.glob('../locales/*.yaml', { eager: true, import: 'default' });
 
 /**
  * Object containing all the locale resources, with the locale names as keys and the corresponding
  * strings as values.
  * @type {Record<string, Record<string, string>>}
  */
-export const resources = Object.fromEntries(
-  Object.entries(rawResources).map(([path, resource]) => [
+export const strings = Object.fromEntries(
+  Object.entries(resources).map(([path, resource]) => [
     /** @type {string} */ (path.match(/.+\/(?<locale>.+?)\.yaml$/)?.groups?.locale),
     /** @type {Record<string, string>} */ (resource),
   ]),
 );
 
 /**
- * Load strings and initialize the locales. Consumers can use this function or {@link resources} to
+ * Load strings and initialize the locales. Consumers can use this function or {@link strings} to
  * load the localized strings for their application.
  * @param {object} [init] Initialize options.
  * @param {string} [init.fallbackLocale] Fallback locale.
@@ -24,7 +24,7 @@ export const resources = Object.fromEntries(
  * @see https://vitejs.dev/guide/features.html#glob-import
  */
 export const initLocales = ({ fallbackLocale = 'en', initialLocale = 'en' } = {}) => {
-  Object.entries(resources).forEach(([locale, resource]) => {
+  Object.entries(strings).forEach(([locale, resource]) => {
     // Add `_sui` suffix to avoid collision with app localization
     addMessages(locale, { _sui: resource });
   });
